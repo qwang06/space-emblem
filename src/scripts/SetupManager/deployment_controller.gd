4056c8b635f6421f389d46f03ed183768753a04e
Qwang <qiangsemail@gmail.com>
2025-04-08 22:52:40 -0600

    deploy units UI unit button

--- src/scripts/SetupManager/deployment_controller.gd
+++ src/scripts/SetupManager/deployment_controller.gd
@@ -4,6 +4,7 @@ signal unit_placed(unit: Node2D)
 signal placement_complete()
 
 @export var max_units := 2
+@export var deployment_ui: Control
 
 # Ideally units should come from player's save file, this is just for testing
 var blue_mage: PackedScene = preload("res://src/scenes/Unit/Characters/blue_mage.tscn")
@@ -27,6 +28,7 @@ func begin_setup():
 		or deployment["player"].size() == 0
 	):
 		return
+
 	# loop through deployment_zones["player"] and convert to Vector2i
 	for i in range(deployment["player"].size()):
 		setup_tiles.append(
@@ -35,6 +37,19 @@ func begin_setup():
 		)
 	placed_units.clear()
 	grid_overlay.set_highlighted_tiles(setup_tiles)
+
+	# Display the units in the deployment UI
+	# Create the unit buttons and add them to the UI
+	var button = Button.new()
+	var unit1 = blue_mage.instantiate()
+	button.text = unit1.name
+	button.connect("pressed", Callable(self, "handle_unit_click").bind(unit1))
+
+	deployment_ui.add_child(button)
+
+
+func handle_unit_click(unit: Node2D) -> void:
+	print("Unit clicked: ", unit.name)
 
 
 func handle_tile_click(tile: Vector2i) -> void:
